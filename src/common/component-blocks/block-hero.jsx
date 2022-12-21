a95231ef5b9bf463482714e05cf6b45740af718a
Manfred Obumma <fredobumma@GMAIL.COM>
2022-12-21 05:59:59 +0100

    --bug: refactor the hero block

--- src/common/component-blocks/block-hero.jsx
+++ src/common/component-blocks/block-hero.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Header from "../header";
 import Button from "./../button";
+import Figure from "../figure";
 
 const Hero = () => {
+  const backgroundImage = [
+    "bg-01 !bg-[-8955px_0] bottom-0 h-[105px] right-[-6%] w-[59px] tablet:!bg-[-9093px_0] tablet:bottom-[-3%] tablet:right-[-3%] tablet:h-[154px] tablet:w-[88px] laptop:!bg-[0_0] laptop:bottom-[-3%] laptop:right-[-2%] laptop:h-[211px] laptop:w-[211px]",
+    "bg-02 !bg-[-9191px_0] bottom-[-7%] h-[139px] left-[-5%] w-[86px] tablet:!bg-[-9364px_0] tablet:bottom-[-20%] tablet:left-[-4%] tablet:h-[229px] tablet:w-[165px] laptop:!bg-[-221px_0] laptop:bottom-[-20%] laptop:left-[-3%] laptop:h-[336px] laptop:w-[301px]",
+    "bg-03 !bg-[-9287px_0] h-[78px] right-[-4%] top-[-1%] w-[67px] tablet:!bg-[-9539px_0] tablet:right-[-3%] tablet:top-[-3%] tablet:h-[166px] tablet:w-[115px] laptop:!bg-[-532px_0] laptop:right-[-2%] laptop:top-[1%] laptop:h-[166px] laptop:w-[165px]",
+    "bg-04 !bg-[-9024px_0] top-[30%] h-[114px] left-[-6%] w-[59px] tablet:!bg-[-9664px_0] tablet:left-[-3%] tablet:top-[-2%] tablet:h-[174px] tablet:w-[118px] laptop:!bg-[-707px_0] laptop:left-[-2%] laptop:top-[1%] laptop:h-[174px] laptop:w-[173px]",
+  ];
+
   return (
     <section className="bg-hero py-12 relative text-center tablet:py-16 laptop:py-28">
       <Header
@@ -16,10 +24,9 @@ const Hero = () => {
         textClasses="mb-6 mt-4 text-center text-base tablet:my-8"
       />
       <Button label="Let's sell" />
-      <span className="sprite bg-01 absolute !bg-[-8955px_0] bottom-0 h-[105px] right-[-6%] w-[59px] tablet:!bg-[-9093px_0] tablet:bottom-[-3%] tablet:right-[-3%] tablet:h-[154px] tablet:w-[88px] laptop:!bg-[0_0] laptop:bottom-[-3%] laptop:right-[-2%] laptop:h-[211px] laptop:w-[211px]"></span>
-      <span className="sprite bg-02 absolute !bg-[-9191px_0] bottom-[-7%] h-[139px] left-[-5%] w-[86px] tablet:!bg-[-9364px_0] tablet:bottom-[-20%] tablet:left-[-4%] tablet:h-[229px] tablet:w-[165px] laptop:!bg-[-221px_0] laptop:bottom-[-20%] laptop:left-[-3%] laptop:h-[336px] laptop:w-[301px]"></span>
-      <span className="sprite bg-03 absolute !bg-[-9287px_0] h-[78px] right-[-4%] top-[-1%] w-[67px] tablet:!bg-[-9539px_0] tablet:right-[-3%] tablet:top-[-3%] tablet:h-[166px] tablet:w-[115px] laptop:!bg-[-532px_0] laptop:right-[-2%] laptop:top-[1%] laptop:h-[166px] laptop:w-[165px]"></span>
-      <span className="sprite bg-04 absolute !bg-[-9024px_0] top-[30%] h-[114px] left-[-6%] w-[59px] tablet:!bg-[-9664px_0] tablet:left-[-3%] tablet:top-[-2%] tablet:h-[174px] tablet:w-[118px] laptop:!bg-[-707px_0] laptop:left-[-2%] laptop:top-[1%] laptop:h-[174px] laptop:w-[173px]"></span>
+      {backgroundImage.map((image, index) => (
+        <Figure key={index} classes={`sprite absolute ${image}`} />
+      ))}
     </section>
   );
 };
