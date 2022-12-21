9ac40cfc0617ed50970d3f97e34129967947f8f6
Manfred Obumma <fredobumma@GMAIL.COM>
2022-12-21 05:41:18 +0100

    --bug: refactor the showcase block

--- src/common/component-blocks/block-showcase.jsx
+++ src/common/component-blocks/block-showcase.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 import Header from "../header";
+import Figure from "../figure";
 
 const Showcase = () => {
+  const cards = [
+    {
+      title: "Moonbirds",
+      text: "Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
+      spriteImage:
+        "moonbirds !bg-[-6974px_0] tablet:!bg-[-4630px_0] bigTab:!bg-[-6974px_0] desktop:!bg-[-4630px_0]",
+    },
+    {
+      title: "Clone X - X Takashi Murakami",
+      text: " Magna in condimentum praesent pretium vitae mattis facilisis nunc. Lectus in ac ac.",
+      spriteImage:
+        "takashi !bg-[-6722px_0] tablet:!bg-[-4208px_0] bigTab:!bg-[-6722px_0] desktop:!bg-[-4208px_0]",
+    },
+    {
+      title: "Mutant Ape Yacht Club",
+      text: "Sit aenean porttitor sit lectus. Adipiscing egestas etiam eget cursus nulla risus euismod. Netus.",
+      spriteImage:
+        "mutantApe !bg-[-6471px_0] tablet:!bg-[-3786px_0] bigTab:!bg-[-6471px_0] desktop:!bg-[-3786px_0]",
+    },
+    {
+      title: `"MOAR" by Joan Cornella`,
+      text: "Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
+      spriteImage:
+        "moar !bg-[-7728px_0] tablet:!bg-[-3364px_0] bigTab:!bg-[-7728px_0] desktop:!bg-[-3364px_0]",
+    },
+    {
+      title: "Doodles",
+      text: "Magna in condimentum praesent pretium vitae mattis facilisis nunc. Lectus in ac ac.",
+      spriteImage:
+        "doodles !bg-[-7476px_0] tablet:!bg-[-2942px_0] bigTab:!bg-[-7476px_0] desktop:!bg-[-2942px_0]",
+    },
+    {
+      title: "KIWAMI Genesis",
+      text: "Sit aenean porttitor sit lectus. Adipiscing egestas etiam eget cursus nulla risus euismod. Netus.",
+      spriteImage:
+        "kiwami !bg-[-7225px_0] tablet:!bg-[-2520px_0] bigTab:!bg-[-7225px_0] desktop:!bg-[-2520px_0]",
+    },
+    {
+      title: "Arcade Land",
+      text: "Sit aenean porttitor sit lectus. Adipiscing egestas etiam eget cursus nulla risus euismod. Netus.",
+      spriteImage:
+        "arcade !bg-[-5718px_0] tablet:!bg-[-2098px_0] bigTab:!bg-[-5718px_0] desktop:!bg-[-2098px_0]",
+    },
+    {
+      title: "Goons of Balatroon",
+      text: "Magna in condimentum praesent pretium vitae mattis facilisis nunc. Lectus in ac ac.",
+      spriteImage:
+        "goons !bg-[-5969px_0] tablet:!bg-[-1676px_0] bigTab:!bg-[-5969px_0] desktop:!bg-[-1676px_0]",
+    },
+    {
+      title: "Meta Toy DragonZ",
+      text: "Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis fringilla pulvinar nibh diam sit.",
+      spriteImage:
+        "meta !bg-[-6220px_0] tablet:!bg-[-1254px_0] bigTab:!bg-[-6220px_0] desktop:!bg-[-1254px_0]",
+    },
+  ];
+
   return (
     <section className="py-12 tablet:py-16 laptop:py-20">
       <Header
@@ -15,96 +73,20 @@ const Showcase = () => {
         textClasses="mb-8 mt-2 text-secondary text-sm tablet:text-base tablet:mb-12 tablet:mt-4 laptop:mb-16"
       />
       <div className="gap-x-4 gap-y-8 grid bigTab:gap-y-12 bigTab:grid-cols-3 laptop:gap-x-6 desktop:gap-8 desktop:gap-y-16">
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite moonbirds !bg-[-6974px_0] h-sprite w-sp-x1 tablet:!bg-[-4630px_0] tablet:w-sprite bigTab:!bg-[-6974px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-4630px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Moonbirds
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis
-            fringilla pulvinar nibh diam sit.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite takashi !bg-[-6722px_0] h-sprite w-sp-x1 tablet:!bg-[-4208px_0] tablet:w-sprite bigTab:!bg-[-6722px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-4208px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Clone X - X Takashi Murakami
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Magna in condimentum praesent pretium vitae mattis facilisis nunc.
-            Lectus in ac ac.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite mutantApe !bg-[-6471px_0] h-sprite w-sp-x1 tablet:!bg-[-3786px_0] tablet:w-sprite bigTab:!bg-[-6471px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-3786px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Mutant Ape Yacht Club
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Sit aenean porttitor sit lectus. Adipiscing egestas etiam eget
-            cursus nulla risus euismod. Netus.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite moar !bg-[-7728px_0] h-sprite w-sp-x1 tablet:!bg-[-3364px_0] tablet:w-sprite bigTab:!bg-[-7728px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-3364px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            "MOAR" by Joan Cornella
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis
-            fringilla pulvinar nibh diam sit.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite doodles !bg-[-7476px_0] h-sprite w-sp-x1 tablet:!bg-[-2942px_0] tablet:w-sprite bigTab:!bg-[-7476px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-2942px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Doodles
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Magna in condimentum praesent pretium vitae mattis facilisis nunc.
-            Lectus in ac ac.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite kiwami !bg-[-7225px_0] h-sprite w-sp-x1 tablet:!bg-[-2520px_0] tablet:w-sprite bigTab:!bg-[-7225px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-2520px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            KIWAMI Genesis
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Sit aenean porttitor sit lectus. Adipiscing egestas etiam eget
-            cursus nulla risus euismod. Netus.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite arcade !bg-[-5718px_0] h-sprite w-sp-x1 tablet:!bg-[-2098px_0] tablet:w-sprite bigTab:!bg-[-5718px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-2098px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Arcade Land
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Sit aenean porttitor sit lectus. Adipiscing egestas etiam eget
-            cursus nulla risus euismod. Netus.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite goons !bg-[-5969px_0] h-sprite w-sp-x1 tablet:!bg-[-1676px_0] tablet:w-sprite bigTab:!bg-[-5969px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-1676px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Goons of Balatroon
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Magna in condimentum praesent pretium vitae mattis facilisis nunc.
-            Lectus in ac ac.
-          </p>
-        </div>
-        <div className="inline-flex items-center flex-col h-auto">
-          <span className="sprite meta !bg-[-6220px_0] h-sprite w-sp-x1 tablet:!bg-[-1254px_0] tablet:w-sprite bigTab:!bg-[-6220px_0] bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:!bg-[-1254px_0] desktop:scale-x-90 desktop:w-sprite"></span>
-          <h3 className="font-semibold mb-3 mt-4 text-2xl laptop:text-32">
-            Meta Toy DragonZ
-          </h3>
-          <p className="max-w-80% mb-6 text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm">
-            Faucibus odio elementum nulla venenatis, libero. Ullamcorper duis
-            fringilla pulvinar nibh diam sit.
-          </p>
-        </div>
+        {cards.map(({ title, text, spriteImage }, index) => (
+          <div key={index} className="inline-flex items-center flex-col h-auto">
+            <Figure
+              classes={`${spriteImage} sprite h-sprite w-sp-x1 tablet:w-sprite bigTab:scale-x-95 bigTab:w-sp-x1 laptop:scale-x-125 desktop:scale-x-90 desktop:w-sprite`}
+            />
+            <Header
+              type="h3"
+              heading={title}
+              headingClasses="font-semibold mb-3 mt-4 text-2xl laptop:text-32"
+              text={text}
+              textClasses="max-w-80% mb-6 mx-auto text-center text-secondary text-xs tablet:max-w-90% tablet:text-sm"
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/common/figure.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Figure = ({ classes }) => {
+  return <span className={classes}></span>;
+};
+
+export default Figure;
