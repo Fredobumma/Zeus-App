86322ae4823deef3aad4adf04b0c61db337dc36a
Manfred Obumma <fredobumma@GMAIL.COM>
2022-12-23 14:29:53 +0100

    --feat: add path to button click events

--- src/common/button.jsx
+++ src/common/button.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Button = ({ label, ...rest }) => {
+  const navigate = useNavigate();
   const getClasses = Object.keys(rest)
     .map((c) => c)
     .toString()
@@ -9,6 +11,7 @@ const Button = ({ label, ...rest }) => {
   return (
     <button
       className={`active:bg-actFocHov focus:bg-actFocHov hover:bg-actFocHov bg-secondary font-bold font-heading font-lg leading-5 outline-0 rounded-15 text-white px-5 py-3 tablet:py-4 ${getClasses}`}
+      onClick={() => navigate("/plans/earnings")}
     >
       {label}
     </button>
